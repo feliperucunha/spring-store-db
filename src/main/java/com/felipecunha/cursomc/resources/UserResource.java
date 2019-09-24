3ea22bb2e53a32fd5a0451ee9b12110171133565
feliperucunha <feliperuben@live.com>
2019-09-24 14:33:59 -0300

    Implementando o padrão DTO

--- src/main/java/com/felipecunha/cursomc/resources/UserResource.java
+++ src/main/java/com/felipecunha/cursomc/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.felipecunha.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipecunha.cursomc.domain.User;
+import com.felipecunha.cursomc.dto.UserDTO;
 import com.felipecunha.cursomc.services.UserService;
 
 @RestController
@@ -21,12 +21,13 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		//User maria = new User("1", "Maria Brown", "maria@gmail.com");
 		//User alex = new User("2", "Alex Green", "alex@gmail.com");
 		//List<User> list = new ArrayList<>();
 		//list.addAll(Arrays.asList(maria,alex));
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
