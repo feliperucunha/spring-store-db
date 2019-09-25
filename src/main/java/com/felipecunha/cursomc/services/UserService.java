5cf9dec83483de81a12f934a197b215aac786905
feliperucunha <feliperuben@live.com>
2019-09-25 14:59:23 -0300

    Inserir User com Post

--- src/main/java/com/felipecunha/cursomc/services/UserService.java
+++ src/main/java/com/felipecunha/cursomc/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipecunha.cursomc.domain.User;
+import com.felipecunha.cursomc.dto.UserDTO;
 import com.felipecunha.cursomc.repository.UserRepository;
 import com.felipecunha.cursomc.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) { //não está no UserDTO, pois por aqui pode acessar também o banco de dados
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
