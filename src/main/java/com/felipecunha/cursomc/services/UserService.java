942c2e03eb0b98c918fe9aca5737c836f129b656
feliperucunha <feliperuben@live.com>
2019-09-26 13:39:21 -0300

    PUT de user pelo Post

--- src/main/java/com/felipecunha/cursomc/services/UserService.java
+++ src/main/java/com/felipecunha/cursomc/services/UserService.java
@@ -29,6 +29,17 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 
 	public User fromDTO(UserDTO objDto) { //não está no UserDTO, pois por aqui pode acessar também o banco de dados
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
